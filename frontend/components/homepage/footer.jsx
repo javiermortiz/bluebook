348464c320d6323781227daf32e2ffb9130fcddb
Javier Ortiz <javier.ortiz989@gmail.com>
2020-04-06 12:51:56 -0700

    Add footer with personal info on splash page

---
+++ frontend/components/homepage/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => (
+    <div className="footer-container">
+        <div className="footer">
+            <h2 className="footer-name">Javier M Ortiz</h2>
+            <ul className="footer-list">
+                <li><a href="https://www.javiermortiz.com/" target="_blank"><i class="far fa-folder-open"></i><span>Portfolio</span></a></li>
+                <li><a href="https://github.com/javiermortiz" target="_blank"><i class="fab fa-github-square"></i> <span>Github</span></a></li>
+                <li><a href="https://www.linkedin.com/in/javiermortiz/" target="_blank"><i class="fab fa-linkedin"></i><span>LinkedIn</span></a></li>
+                <li><a href="https://angel.co/u/javiermortiz" target="_blank"><i class="fab fa-angellist"></i><span>AngelList</span></a></li>
+            </ul>
+        </div>
+    </div>
+);
+
+export default Footer;
--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import LoginFormContainer from '../session_form/login_form_container'
 import SignupFormContainer from '../session_form/signup_form_container'
+import Footer from './footer';
 
 const Homepage = () => (
     <div className="welcome_page">
         <LoginFormContainer />
         <SignupFormContainer />  
+        <Footer />
     </div>
 );
 
