ca2e96e24af8164186f0bfd09a850907213503d2
Javier Ortiz <javier.ortiz989@gmail.com>
2020-04-09 12:39:02 -0700

    Style sign up button on hover

--- frontend/components/homepage/footer.jsx
+++ frontend/components/homepage/footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => (
         <div className="footer">
             <h2 className="footer-name">Javier M Ortiz</h2>
             <ul className="footer-list">
-                <li><a href="https://www.javiermortiz.com/" target="_blank"><i class="far fa-folder-open"></i><span>Portfolio</span></a></li>
-                <li><a href="https://github.com/javiermortiz" target="_blank"><i class="fab fa-github-square"></i> <span>Github</span></a></li>
-                <li><a href="https://www.linkedin.com/in/javiermortiz/" target="_blank"><i class="fab fa-linkedin"></i><span>LinkedIn</span></a></li>
-                <li><a href="https://angel.co/u/javiermortiz" target="_blank"><i class="fab fa-angellist"></i><span>AngelList</span></a></li>
+                <li><a href="https://www.javiermortiz.com/" target="_blank"><i className="far fa-folder-open"></i><span>Portfolio</span></a></li>
+                <li><a href="https://github.com/javiermortiz" target="_blank"><i className="fab fa-github-square"></i> <span>Github</span></a></li>
+                <li><a href="https://www.linkedin.com/in/javiermortiz/" target="_blank"><i className="fab fa-linkedin"></i><span>LinkedIn</span></a></li>
+                <li><a href="https://angel.co/u/javiermortiz" target="_blank"><i className="fab fa-angellist"></i><span>AngelList</span></a></li>
             </ul>
         </div>
     </div>
